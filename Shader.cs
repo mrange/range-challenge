397b80bea895eb6f068de73fd090e21e03ef1703
mrange <marten_range@hotmail.com>
2025-12-29 22:39:46 +0100

    GG

--- Shader.cs
+++ Shader.cs
@@ -6,9 +6,10 @@ using static System.Math;
 
 public sealed class Shader : ShaderBase
 {
-  readonly static Vector3 _0  = new (0);
-  readonly static Vector3 _1  = new (1);
-  readonly static Vector3 _255= new (255);
+  readonly static Vector3 _0    = new(0);
+  readonly static Vector3 _1    = new(1);
+  readonly static Vector3 _255  = new(255);
+  readonly static Vector3 _Base = new(-0.7F,-0.2F,0.3F);
 
   static Color ToColor(Vector3 c) 
   {
@@ -16,41 +17,60 @@ public sealed class Shader : ShaderBase
     return new((byte)C.X,(byte)C.Y,(byte)C.Z);
   }
 
-  float   _time;
-  Vector2 _res;
+  Vector2   _res ;
+  Vector2   _x   ;
+  Vector2   _y   ;
 
   protected override void Setup(int weight, int height, double time)
   {
-    _time=(float)time;
     _res =new(weight, height);
+    var (c,s) = SinCos(time);
+    _x = new((float)+c,(float)+s);
+    _y = new((float)-s,(float)+c);
   }
 
   protected override Color Run(int x, int y)
   {
     Vector2 
       c = new (x,y)
-    , p = (c+c-_res)/_res.Y;
+    , p = (c+c-_res)/_res.Y
+    , t
     ;
 
     Vector3
-      col = _0
+      C = _0
+    , P
+    , R = Vector3.Normalize(new(p.X,p.Y,2))
     ;
 
     float
-      b
-    , l
+      z = 0
+    , d = 1
     ;
-    b = _time-(float)Floor(_time)-.5F;
-    b *=b;
-    b = .25F-b;
-    p.Y+=b;
-    l = p.Length() - .5F;
 
-    if (l<0) 
+    int 
+      i
+    ;
+    for(i=0; i<49&&z<4&&d>1e-3; ++i) 
     {
-      col = _1;
+      P=z*R;
+      P.Z-=3;
+      t=new(P.X,P.Z);
+      t=new(Vector2.Dot(t,_x),Vector2.Dot(t,_y));
+      P.X=t.X;
+      P.Z=t.Y;
+      t=new(P.X,P.Y);
+      t=new(Vector2.Dot(t,_x),Vector2.Dot(t,_y));
+      P.X=t.X;
+      P.Y=t.Y;
+      P*=P;
+      d=(float)Sqrt(Sqrt(Vector3.Dot(P,P)))-1;
+      z+=d;
     }
 
-    return ToColor(col);
+    if (z<4) 
+      C=.5F*(_1+Vector3.Sin(_Base-new Vector3(i/33F+2*(p.X+p.Y))));
+
+    return ToColor(C);
   }
 }
