d1fc3082d2ea1aecfeffb72fde0b4d35a810e25b
mrange <marten_range@hotmail.com>
2025-12-29 21:26:12 +0100

    GG

--- Infrastructure/ShaderBase.cs
+++ Infrastructure/ShaderBase.cs
@@ -1,4 +1,4 @@
-using Spectre.Tui;
+﻿using Spectre.Tui;
 
 namespace TermShader.Infrastructure;
 
@@ -6,15 +6,22 @@ public abstract class ShaderBase
 {
     public void Render(RenderContext context, TimeSpan elapsed)
     {
-        for (var x = 0; x < context.Viewport.Width; x++)
+        int width = context.Viewport.Width;
+        int height = context.Viewport.Height;
+        for (var x = 0; x < width; x++)
         {
-            for (var y = 0; y < context.Viewport.Height; y++)
+            for (var y = 0; y < height; y++)
             {
-                context.GetCell(x, y)
-                    ?.SetBackground(Run(x, y, elapsed));
+                var c = context.GetCell(x,y);
+                if (c is not null)
+                {
+                  c.SetSymbol('▀');
+                  c.SetForeground(Run(x, y+y+0, width, height+height, elapsed));
+                  c.SetBackground(Run(x, y+y+1, width, height+height, elapsed));
+                }
             }
         }
     }
 
-    protected abstract Color Run(int x, int y, TimeSpan elapsed);
+    protected abstract Color Run(int x, int y, int weight, int height, TimeSpan elapsed);
 }
--- Shader.cs
+++ Shader.cs
@@ -1,10 +1,23 @@
 using Spectre.Tui;
+using System.Numerics;
 using TermShader.Infrastructure;
+
+using static Spectre.Tui.Color;
 
 public sealed class Shader : ShaderBase
 {
-    protected override Color Run(int x, int y, TimeSpan elapsed)
-    {
-        return Random.Shared.Next(0, 255);
-    }
+  protected override Color Run(int x, int y, int w, int h, TimeSpan elapsed)
+  {
+    Vector2 
+      c = new (x,y)
+    , r = new (w,h)
+    , p = (c+c-r)/r.Y;
+    ;
+
+    float
+      l = p.Length() - 1.0F
+    ;
+
+    return l<0?White:Black;
+  }
 }
