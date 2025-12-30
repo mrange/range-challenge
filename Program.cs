506141d3f92ba5b5ee66f550aea07b84ad19bd82
Patrik Svensson <patrik@patriksvensson.se>
2025-12-30 10:57:32 +0100

    Add credits

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿using Spectre.Tui;
 using System.Diagnostics;
 
-var terminal = Terminal.Create();
+using var terminal = Terminal.Create();
 var renderer = new Renderer(terminal);
 var shader = new BoxShader();
 var sw = Stopwatch.StartNew();
+
+var text = Text.FromMarkup("Created by [yellow]Mårten Rånge[/]");
+var text2 = Text.FromMarkup("Spectre.Tui");
 
 while (true)
 {
     renderer.Draw((ctx, elapsed) =>
     {
         shader.Render(ctx, sw.Elapsed.TotalSeconds);
+
+        var fps = (int)(1 / elapsed.TotalSeconds);
+        ctx.Render(Text.FromMarkup($"FPS: [blue]{fps}[/]"));
+        
+        ctx.Render(text2, new Rectangle(0, ctx.Viewport.Bottom - 1, text2.GetWidth(), 1));
+        ctx.Render(text, new Rectangle(ctx.Viewport.Right - text.GetWidth(), ctx.Viewport.Bottom - 1, text.GetWidth(), 1));
     });
     
     // Check for exit
